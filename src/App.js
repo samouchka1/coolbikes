a89a580d96912768f1cf2f6936ad328a37047aee
samouchka1 <mchio421@protonmail.com>
2022-11-27 22:50:26 -0500

    added Road content

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import FixedGear from './components/FixedGear';
 import Gravel from './components/Gravel';
+import Road from './components/Road';
 import About from './components/About';
 import Footer from './components/Footer';
 
@@ -27,6 +28,7 @@ function App() {
             <Route path="/" element={<Welcome />} />
             <Route path="/fixedgear" element={<FixedGear />} />
             <Route path="/gravel" element={<Gravel />} />
+            <Route path="/road" element={<Road />} />
             <Route path="/about" element={<About />} />
           </Routes>
           <Footer />
